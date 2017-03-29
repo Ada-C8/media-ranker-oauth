e4c2e2c1c9af1831a65e856766a72e5555419e69
Dan Roberts <dan@adadevelopersacademy.org>
2017-03-29 15:16:24 -0700

    Test/implement validations on Piece

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -1,4 +1,10 @@
 class Piece < ApplicationRecord
   has_many :votes
   has_many :ranking_users, through: :votes, source: :user
+
+  validates :category,  presence: true,
+                        inclusion: { in: %w(album book movie) }
+
+  validates :title, presence: true,
+                    uniqueness: { scope: :category }
 end
--- test/models/piece_test.rb
+++ test/models/piece_test.rb
@@ -18,4 +18,49 @@ class PieceTest < ActiveSupport::TestCase
       end
     end
   end
+
+  describe "validations" do
+    it "allows the three valid categories" do
+      valid_categories = ['album', 'book', 'movie']
+      valid_categories.each do |category|
+        piece = Piece.new(title: "test", category: category)
+        piece.valid?.must_equal true
+      end
+    end
+
+    it "rejects invalid categories" do
+      invalid_categories = ['cat', 'dog', 'phd thesis', 1337, nil]
+      invalid_categories.each do |category|
+        piece = Piece.new(title: "test", category: category)
+        piece.valid?.must_equal false
+        piece.errors.messages.must_include :category
+      end
+    end
+
+    it "requires a title" do
+      piece = Piece.new(category: 'ablum')
+      piece.valid?.must_equal false
+      piece.errors.messages.must_include :title
+    end
+
+    it "requires unique names w/in categories" do
+      category = 'album'
+      title = 'test title'
+      piece1 = Piece.new(title: title, category: category)
+      piece1.save!
+
+      piece2 = Piece.new(title: title, category: category)
+      piece2.valid?.must_equal false
+      piece2.errors.messages.must_include :title
+    end
+
+    it "does not require a unique name if the category is different" do
+      title = 'test title'
+      piece1 = Piece.new(title: title, category: 'album')
+      piece1.save!
+
+      piece2 = Piece.new(title: title, category: 'book')
+      piece2.valid?.must_equal true
+    end
+  end
 end
