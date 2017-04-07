06b067d6c349d9d98a58a6d8b24dcaed16d15785
Dan Roberts <dan@adadevelopersacademy.org>
2017-04-07 11:51:38 -0700

    Add database seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'csv'
+media_file = Rails.root.join('db', 'media_seeds.csv')
+
+CSV.foreach(media_file, headers: true, header_converters: :symbol, converters: :all) do |row|
+  data = Hash[row.headers.zip(row.fields)]
+  puts data
+  Work.create!(data)
+end
